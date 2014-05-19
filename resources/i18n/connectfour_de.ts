11b196167b25c1dbf25bb7a38e3253818cad2b1c
David Greisler <kontakt@davidgreisler.de>
2014-05-19 02:19:30 +0200

    Updated translation.

--- resources/i18n/connectfour_de.ts
+++ resources/i18n/connectfour_de.ts
@@ -12,95 +12,272 @@
 <context>
     <name>GUI::Actions::Game</name>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="116"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="169"/>
         <source>&amp;New game</source>
         <translation>&amp;Neues Spiel</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="117"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="170"/>
         <source>Start a new game.</source>
         <translation>Starte ein neues Spiel.</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="119"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="172"/>
         <source>&amp;End game</source>
         <translation>Spiel &amp;beenden</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="120"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="173"/>
         <source>End the current game.</source>
-        <translation>Beendet das aktuelle Spiel.</translation>
+        <translation>Beende das aktuelle Spiel.</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="122"/>
         <source>&amp;Load game</source>
-        <translation>&amp;Lade Spielstand</translation>
+        <translation type="obsolete">&amp;Lade Spielstand</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="123"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="175"/>
+        <source>&amp;Load game ...</source>
+        <translation>Spiel &amp;laden</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="176"/>
         <source>Load a saved game.</source>
         <translation>Lade ein gespeichertes Spiel.</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="125"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="178"/>
         <source>&amp;Save game</source>
         <translation>Spiel &amp;speichern</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="126"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="179"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="182"/>
         <source>Save the current game.</source>
-        <translation>Speichert das aktuelle Spiel.</translation>
+        <translation>Speichere das aktuelle Spiel.</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="128"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="181"/>
+        <source>&amp;Save game as ...</source>
+        <translation>Spiel speichern &amp;unter ...</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="184"/>
         <source>Show &amp;Highscores</source>
         <translation>&amp;Zeige Bestenliste</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="129"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="185"/>
         <source>Show highscore list.</source>
-        <translation>Zeigt die Bestenliste an.</translation>
+        <translation type="unfinished">Zeige die Bestenliste an.</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="131"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="187"/>
         <source>E&amp;xit</source>
         <translation>B&amp;eenden</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Game.cpp" line="132"/>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="188"/>
         <source>Exit game.</source>
-        <translation>Beendet das Spiel.</translation>
+        <translation>Beende das Spiel.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Game.cpp" line="190"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Spiel</translation>
+    </message>
+</context>
+<context>
+    <name>GUI::Actions::Help</name>
+    <message>
+        <location filename="../../src/GUI/Actions/Help.cpp" line="111"/>
+        <source>&amp;About</source>
+        <translation>&amp;Über ...</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Help.cpp" line="112"/>
+        <source>Shows information about this game.</source>
+        <translation>Zeige Informationen über das Spiel an.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Help.cpp" line="114"/>
+        <source>&amp;About Qt ...</source>
+        <translation>Über &amp;Qt ...</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Help.cpp" line="115"/>
+        <source>Shows information about Qt.</source>
+        <translation>Zeige Informationen über Qt an.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Help.cpp" line="117"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hilfe</translation>
+    </message>
+</context>
+<context>
+    <name>GUI::Actions::Move</name>
+    <message>
+        <location filename="../../src/GUI/Actions/Move.cpp" line="110"/>
+        <source>&amp;Undo last move</source>
+        <translation>Letzten Zug &amp;rückgängig machen</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Move.cpp" line="111"/>
+        <source>Undoes last move.</source>
+        <translation>Macht den letzten Spielzug rückgängig.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Move.cpp" line="113"/>
+        <source>&amp;Show hint</source>
+        <translation>&amp;Zeige Hinweis</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Move.cpp" line="114"/>
+        <source>Shows a hint for the next move.</source>
+        <translation>Zeigt eine Empfehlung für den nächsten Spielzug an.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Move.cpp" line="116"/>
+        <source>&amp;Move</source>
+        <translation>Spiel&amp;zug</translation>
+    </message>
+</context>
+<context>
+    <name>GUI::Actions::Replay</name>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="199"/>
+        <source>&amp;Load replay ...</source>
+        <translation>&amp;Lade Wiederholung ...</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="200"/>
+        <source>Load a replay.</source>
+        <translation>Lade eine Wiederholung.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="202"/>
+        <source>&amp;Save replay ...</source>
+        <translation>Wiederholung speichern ...</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="203"/>
+        <source>Save a replay of the current game.</source>
+        <translation>Speichere die Wiederholung des aktuellen Spiels.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="205"/>
+        <source>&amp;Next move</source>
+        <translation>&amp;Nächster Spielzug</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="206"/>
+        <source>Step to the next move.</source>
+        <translation>Nächsten Spielzug zeigen.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="208"/>
+        <source>&amp;Previous move</source>
+        <translation>&amp;Vorheriger Spielzug</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="209"/>
+        <source>Step to the previous move.</source>
+        <translation>Vorherigen Spielzug zeigen.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="211"/>
+        <source>&amp;Jump to start</source>
+        <translation>Springe zum &amp;Anfang</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="212"/>
+        <source>Jump to the start of the replay.</source>
+        <translation>Springe zum Anfang der Spielwiederholung.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="214"/>
+        <source>&amp;Jump to end</source>
+        <translation>Springe zum &amp;Ende</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="215"/>
+        <source>Jump to the end of the replay.</source>
+        <translation>Springe zum Ende der Spielwiederholung.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Replay.cpp" line="217"/>
+        <source>&amp;Replay</source>
+        <translation>&amp;Wiederholung</translation>
     </message>
 </context>
 <context>
     <name>GUI::Actions::Settings</name>
     <message>
-        <location filename="../../src/GUI/Actions/Settings.cpp" line="99"/>
+        <location filename="../../src/GUI/Actions/Settings.cpp" line="155"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Settings.cpp" line="156"/>
+        <source>&amp;Language</source>
+        <translation>&amp;Sprache</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/Settings.cpp" line="158"/>
         <source>Open &amp;settings ...</source>
         <translation>Öffne &amp;Einstellungen ...</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/Actions/Settings.cpp" line="100"/>
+        <location filename="../../src/GUI/Actions/Settings.cpp" line="159"/>
         <source>Open the settings dialog.</source>
-        <translation>Öffnet den Einstellungsdialog.</translation>
+        <translation>Öffne den Einstellungsdialog.</translation>
+    </message>
+</context>
+<context>
+    <name>GUI::Actions::View</name>
+    <message>
+        <location filename="../../src/GUI/Actions/View.cpp" line="93"/>
+        <source>&amp;Fullscreen mode</source>
+        <translation>&amp;Vollbildmodus</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/View.cpp" line="94"/>
+        <source>Toggles fullscreen mode on/off.</source>
+        <translation>Schalte den Vollbildmodus an/aus.</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/Actions/View.cpp" line="96"/>
+        <source>&amp;View</source>
+        <translation>&amp;Ansicht</translation>
     </message>
 </context>
 <context>
     <name>GUI::MainMenuBar</name>
     <message>
-        <location filename="../../src/GUI/MainMenuBar.cpp" line="51"/>
         <source>&amp;Game</source>
-        <translation>&amp;Spiel</translation>
+        <translation type="obsolete">&amp;Spiel</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/MainMenuBar.cpp" line="53"/>
         <source>&amp;Settings</source>
-        <translation>&amp;Einstellungen</translation>
+        <translation type="obsolete">&amp;Einstellungen</translation>
     </message>
     <message>
-        <location filename="../../src/GUI/MainMenuBar.cpp" line="55"/>
         <source>&amp;Languages</source>
-        <translation>&amp;Sprachen</translation>
+        <translation type="obsolete">&amp;Sprachen</translation>
+    </message>
+</context>
+<context>
+    <name>GUI::MainWindow</name>
+    <message>
+        <location filename="../../src/GUI/MainWindow.cpp" line="37"/>
+        <source>Connect four</source>
+        <translation>View gewinnt</translation>
+    </message>
+    <message>
+        <location filename="../../src/GUI/MainWindow.cpp" line="38"/>
+        <source>Ready.</source>
+        <translation>Bereit.</translation>
     </message>
 </context>
 </TS>
