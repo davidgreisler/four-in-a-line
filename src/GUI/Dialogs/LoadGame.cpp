d7fb2687668f65a6383ea12105c02d8b6d780e47
David Greisler <kontakt@davidgreisler.de>
2014-06-24 13:03:08 +0200

    Implemented load game dialog.

---
+++ src/GUI/Dialogs/LoadGame.cpp
@@ -0,0 +1,131 @@
+#include "LoadGame.hpp"
+#include "../Game.hpp"
+
+#include <QPushButton>
+#include <QEvent>
+#include <QGridLayout>
+#include <QDialogButtonBox>
+
+namespace GUI
+{
+namespace Dialogs
+{
+
+/**
+ * Creates a new load game dialog for the given game.
+ *
+ * @param game Game to load.
+ * @param parent Parent widget.
+ */
+LoadGame::LoadGame(QSharedPointer< ::GUI::Game> game, QWidget *parent) :
+	QDialog(parent), game(game)
+{
+	this->layout = new QVBoxLayout;
+	this->setLayout(this->layout);
+
+	this->gameSetupWidget = new Widgets::GameSetup(this);
+	this->layout->addWidget(this->gameSetupWidget);
+
+	this->buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+	this->layout->addWidget(this->buttonBox);
+
+	this->loadGameButton = this->buttonBox->button(QDialogButtonBox::Ok);
+
+	// Load game settings.
+
+	auto boardWidget = this->gameSetupWidget->getBoardConfigurationWidget();
+	boardWidget->setNumberOfRows(game->getGameEngine()->getBoard()->getNumberOfRows());
+	boardWidget->setNumberOfColumns(game->getGameEngine()->getBoard()->getNumberOfColumns());
+
+	auto timeLimitWidget = this->gameSetupWidget->getTimeLimitConfigurationWidget();
+	timeLimitWidget->setHasTimeLimit(game->getGameEngine()->hasTimeLimit());
+	timeLimitWidget->setTimeLimit(game->getGameEngine()->getTimeLimit());
+	timeLimitWidget->setTimeoutAction(game->getGameEngine()->getTimeoutAction());
+
+	auto gameWidget = this->gameSetupWidget->getGameConfigurationWidget();
+	gameWidget->setAllowHint(game->isHintAllowed());
+	gameWidget->setAllowUndo(game->isUndoAllowed());
+	gameWidget->setSaveHighscore(game->isSavingHighscore());
+
+	auto firstPlayerWidget = this->gameSetupWidget->getFirstPlayerConfigurationWidget();
+	firstPlayerWidget->setPlayerName(game->getFirstPlayer()->getName());
+	firstPlayerWidget->setPlayerType(::GUI::Widgets::PlayerConfiguration::PlayerType::HUMAN_PLAYER);
+
+	auto secondPlayerWidget = this->gameSetupWidget->getSecondPlayerConfigurationWidget();
+	secondPlayerWidget->setPlayerName(game->getSecondPlayer()->getName());
+	secondPlayerWidget->setPlayerType(
+				::GUI::Widgets::PlayerConfiguration::PlayerType::ARTIFICIAL_INTELLIGENCE);
+
+	// Disable widgets.
+
+	boardWidget->setEnabled(false);
+	timeLimitWidget->setEnabled(false);
+	gameWidget->setEnabled(false);
+	firstPlayerWidget->setEnabled(true);
+	secondPlayerWidget->setEnabled(true);
+
+	this->retranslateUI();
+
+	this->connect(this->buttonBox, &QDialogButtonBox::accepted, this, &LoadGame::accept);
+	this->connect(this->buttonBox, &QDialogButtonBox::rejected, this, &LoadGame::reject);
+	this->connect(this->gameSetupWidget, &Widgets::GameSetup::configurationChanged,
+				  this, &LoadGame::updateLoadGameButton);
+}
+
+/**
+ * Frees all used resoures.
+ */
+LoadGame::~LoadGame()
+{
+
+}
+
+/**
+ * Replaces the placeholder players in the game with players created according to the configuration.
+ *
+ * @param playerFactory Player factory used to create the players.
+ */
+void LoadGame::replacePlayers(PlayerFactory& playerFactory)
+{
+	auto firstPlayer =
+			this->gameSetupWidget->getFirstPlayerConfigurationWidget()->createPlayer(playerFactory);
+	auto secondPlayer =
+			this->gameSetupWidget->getSecondPlayerConfigurationWidget()->createPlayer(playerFactory);
+
+	this->game->replacePlayers(firstPlayer, secondPlayer);
+}
+
+/**
+ * Enables/disabled the load game button according to whether the game setup is valid or not.
+ */
+void LoadGame::updateLoadGameButton()
+{
+	this->loadGameButton->setEnabled(this->gameSetupWidget->isValid());
+}
+
+/**
+ * Retranslates all strings.
+ */
+void LoadGame::retranslateUI()
+{
+	this->setWindowTitle(this->tr("Load game"));
+	this->loadGameButton->setText(this->tr("Start game"));
+}
+
+/**
+ * Retranslates strings when the application's language has been changed.
+ *
+ * @param event Change event.
+ */
+void LoadGame::changeEvent(QEvent* event)
+{
+	if (event->type() == QEvent::LanguageChange)
+	{
+		this->retranslateUI();
+	}
+
+	QDialog::changeEvent(event);
+}
+
+}
+}
---
+++ src/GUI/Dialogs/LoadGame.hpp
@@ -0,0 +1,83 @@
+#ifndef GUI_DIALOGS_LOADGAME_HPP
+#define GUI_DIALOGS_LOADGAME_HPP
+
+#include "../PlayerFactory.hpp"
+#include "../Widgets/BoardConfiguration.hpp"
+#include "../Widgets/TimeLimitConfiguration.hpp"
+#include "../Widgets/PlayerConfiguration.hpp"
+#include "../Widgets/GameConfiguration.hpp"
+#include "../Widgets/GameSetup.hpp"
+
+#include <QDialog>
+#include <QSharedPointer>
+
+class QDialogButtonBox;
+class QVBoxLayout;
+
+namespace GUI
+{
+
+class Game;
+
+namespace Dialogs
+{
+
+/**
+ * Dialog showing the loaded game settings and allowing the user to configure players and then
+ * start the game.
+ *
+ * This dialog shows the game settings like the number of rows/columns, etc. like the new game
+ * dialog but they are read-only. The dialog allows the user to configure the 2 players though.
+ *
+ * After they have been configured and the player clicks the load game button, the placeholder
+ * players in the game are replaced by players created according to the settings defined by the
+ * user and the dialog is closed.
+ */
+class LoadGame : public QDialog
+{
+		Q_OBJECT
+	public:
+		explicit LoadGame(QSharedPointer< ::GUI::Game> game, QWidget *parent = 0);
+		virtual ~LoadGame();
+
+		void replacePlayers(PlayerFactory& playerFactory);
+
+	private slots:
+		void updateLoadGameButton();
+
+	private:
+		Q_DISABLE_COPY(LoadGame)
+
+		void changeEvent(QEvent* event);
+		void retranslateUI();
+
+		/**
+		 * Layout of the dialog.
+		 */
+		QVBoxLayout* layout;
+
+		/**
+		 * Contains game configuration widgets.
+		 */
+		Widgets::GameSetup* gameSetupWidget;
+
+		/**
+		 * Contains dialog buttons.
+		 */
+		QDialogButtonBox* buttonBox;
+
+		/**
+		 * Button to start the game.
+		 */
+		QPushButton* loadGameButton;
+
+		/**
+		 * The loaded game with placeholder players to replace.
+		 */
+		QSharedPointer< ::GUI::Game> game;
+};
+
+}
+}
+
+#endif // GUI_DIALOGS_LOADGAME_HPP
