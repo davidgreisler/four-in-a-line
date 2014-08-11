805fb1cc6503ffa081f5dc90063b9c2197a4de6d
David Greisler <kontakt@davidgreisler.de>
2014-08-11 19:54:53 +0200

    Added chat widget.

--- src/GUI/Widgets/Chat.cpp
+++ src/GUI/Widgets/Chat.cpp
@@ -1,5 +1,13 @@
 #include "Chat.hpp"
 
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QPushButton>
+#include <QTextTable>
+#include <QScrollBar>
+
 namespace GUI
 {
 namespace Widgets
@@ -13,11 +21,32 @@ namespace Widgets
 Chat::Chat(QWidget *parent) :
     QWidget(parent)
 {
-	this->setAutoFillBackground(true);
+	this->messagesEdit = new QTextEdit(this);
+	this->messagesEdit->setContextMenuPolicy(Qt::NoContextMenu);
+	this->messagesEdit->setReadOnly(true);
 
-	QPalette palette = this->palette();
-	palette.setColor(this->backgroundRole(), Qt::yellow);
-	this->setPalette(palette);
+	this->format.setBorder(0);
+
+	this->inputEdit = new QLineEdit(this);
+
+	this->sendButton = new QPushButton(this);
+
+	this->inputLayout = new QHBoxLayout();
+	this->inputLayout->addWidget(this->inputEdit);
+	this->inputLayout->addWidget(this->sendButton);
+
+	this->verticalLayout = new QVBoxLayout(this);
+	this->verticalLayout->setContentsMargins(0, 0, 0, 0);
+	this->verticalLayout->addWidget(this->messagesEdit);
+	this->verticalLayout->addLayout(this->inputLayout);
+
+	this->setLayout(this->verticalLayout);
+
+	this->connect(this->inputEdit, &QLineEdit::returnPressed, this, &Chat::sendMessage);
+	this->connect(this->sendButton, &QPushButton::clicked, this, &Chat::sendMessage);
+
+	this->retranslateUI();
+	this->setUserName("User");
 }
 
 /**
@@ -26,6 +55,86 @@ Chat::Chat(QWidget *parent) :
 Chat::~Chat()
 {
 
+}
+
+/**
+ * Returns the user name.
+ *
+ * @return The user name.
+ */
+QString Chat::getUserName() const
+{
+	return this->userName;
+}
+
+/**
+ * Sets the name of the user.
+ *
+ * @param userName The new user name.
+ */
+void Chat::setUserName(const QString& userName)
+{
+	this->userName = userName;
+}
+
+/**
+ * Appends given message to message widget.
+ *
+ * @param playerName Player name.
+ * @param message The message.
+ */
+void Chat::appendMessage(QString playerName, QString message)
+{
+	QTextCursor cursor(this->messagesEdit->textCursor());
+	cursor.movePosition(QTextCursor::End);
+
+	QTextTable* table = cursor.insertTable(1, 2, this->format);
+	table->cellAt(0, 0).firstCursorPosition().insertText(playerName + ':');
+	table->cellAt(0, 1).firstCursorPosition().insertText(message);
+
+	QScrollBar* scrollBar = this->messagesEdit->verticalScrollBar();
+	scrollBar->setValue(scrollBar->maximum());
+}
+
+/**
+ * Sends the message in the input edit.
+ */
+void Chat::sendMessage()
+{
+	QString messageText = this->inputEdit->text();
+
+	if (!messageText.isEmpty())
+	{
+		// Send message here.
+
+		/// @todo Implement.
+
+		this->appendMessage(this->userName, messageText);
+		this->inputEdit->clear();
+	}
+}
+
+/**
+ * Retranslates all strings.
+ */
+void Chat::retranslateUI()
+{
+	this->sendButton->setText(this->tr("Send"));
+}
+
+/**
+ * Retranslates strings when the application's language has been changed.
+ *
+ * @param event Change event.
+ */
+bool Chat::event(QEvent* event)
+{
+	if (event->type() == QEvent::LanguageChange)
+	{
+		this->retranslateUI();
+	}
+
+	return QWidget::event(event);
 }
 
 }
--- src/GUI/Widgets/Chat.hpp
+++ src/GUI/Widgets/Chat.hpp
@@ -2,6 +2,13 @@
 #define GUI_WIDGETS_CHAT_HPP
 
 #include <QWidget>
+#include <QTextTableFormat>
+
+class QVBoxLayout;
+class QHBoxLayout;
+class QLineEdit;
+class QTextEdit;
+class QPushButton;
 
 namespace GUI
 {
@@ -10,23 +17,61 @@ namespace Widgets
 
 /**
  * Provides a message list and an input field for writing chat messages.
- *
- * @todo Implement.
  */
 class Chat : public QWidget
 {
 		Q_OBJECT
 	public:
-		explicit Chat(QWidget *parent = 0);
+		explicit Chat(QWidget *parent = nullptr);
 		virtual ~Chat();
 
-	signals:
+		QString getUserName() const;
+		void setUserName(const QString& value);
 
-	public slots:
+	protected slots:
+		void appendMessage(QString playerName, QString message);
+		void sendMessage();
 
 	private:
 		Q_DISABLE_COPY(Chat)
 
+		void retranslateUI();
+		bool event(QEvent* event);
+
+		/**
+		 * Vertical layout.
+		 */
+		QVBoxLayout* verticalLayout;
+
+		/**
+		 * Layout for the message input and the send button.
+		 */
+		QHBoxLayout* inputLayout;
+
+		/**
+		 * Chat messages edit.
+		 */
+		QTextEdit* messagesEdit;
+
+		/**
+		 * Message input edit.
+		 */
+		QLineEdit* inputEdit;
+
+		/**
+		 * Send button.
+		 */
+		QPushButton* sendButton;
+
+		/**
+		 * Name of the user.
+		 */
+		QString userName;
+
+		/**
+		 * Table format for a message.
+		 */
+		QTextTableFormat format;
 };
 
 }
